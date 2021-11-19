5d5a601a1f58ce28ad0e9404ba39ddb95a6050f7
vlsilver <68173811+vlsilver@users.noreply.github.com>
2021-11-19 11:16:14 +0700

    Return and handle an error

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,9 +1,18 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
-func Hello(name string) string {
-	// Return a greeting that embeds the name in a message.
+// Hello returns a greetings for the names person.
+func Hello(name string) (string, error) {
+	// If no name was given, return an error with a message
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+	// If a name was recieved return a value that embeds the name
+	//in a greeting message.
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message
+	return message, nil
 }
--- hello/hello.go
+++ hello/hello.go
@@ -3,10 +3,24 @@ package main
 import (
 	"fmt"
 	"greetings"
+	"log"
 )
 
 func main() {
-	// Get a greeting message and print it.
-	message := greetings.Hello("Something from greetings module")
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printings
+	// the time, source file, and line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	// Request a greetings messages.
+	message, err := greetings.Hello("")
+	// If an error was returned, print it to the console and
+	// exit the program.
+	if err != nil {
+		log.Fatal(err)
+	}
+	// If no error was returned, print the returned message
+	// to the console
 	fmt.Println(message)
 }
